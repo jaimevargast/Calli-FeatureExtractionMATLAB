19cb6c9aede5707ec39858f8c154d11d446ba784
jaimevargast <jaime.vargas@gmail.com>
2015-05-01 20:28:50 -0700

    Fixed polygon extraction added fsds

---
+++ Eliptic Fourier/fEfourier_batch.m
@@ -0,0 +1,58 @@
+function fEfourier_batch(directory)
+%FEFOURIER_BATCH Summary of this function goes here
+%   Detailed explanation goes here
+
+iNoOfHarmonicsAnalyse = 15;
+bNormaliseSizeState = 1;
+bNormaliseOrientationState = 0;
+
+mkdir(directory,'fourier shape descriptors');
+savepath = strcat(directory,'/fourier shape descriptors/');
+
+polys = dir(directory);
+
+%aux = struct('x',[],'y',[],'hole',[]);
+
+for f = 1:length(polys)
+    
+    fsd = {};
+    
+    [pathstr,name,ext] = fileparts(polys(f).name);
+    
+    
+    % Load the polygon instance
+    if(strcmp(ext,'.mat'))
+        source = strcat(name,ext);
+        fsd_saveas = strcat(savepath,name,'_fsd.mat');
+        
+        load(source);
+        
+        if exist('polygon','var')
+            outline = [polygon(1).x polygon(1).y];
+            % Extract FSD
+            rFSDs = fEfourier(outline, iNoOfHarmonicsAnalyse, bNormaliseSizeState, bNormaliseOrientationState);
+            fsd = [];
+            for col = 1:size(rFSDs,2)
+                fsd = [fsd rFSDs(:,col).'];
+            end
+            save(fsd_saveas,'fsd');
+            
+        end
+        
+        
+    end
+    
+    
+    
+    
+    
+    
+    % Extract FSD
+    %             rFSDs = fEfourier(outline, iNoOfHarmonicsAnalyse, bNormaliseSizeState, bNormaliseOrientationState);
+    %
+    %             save(fsd_saveas,'rFSDs');
+    
+end
+
+end
+
--- png2poly/png2poly_batch.m
+++ png2poly/png2poly_batch.m
@@ -1,4 +1,6 @@
 function png2poly_batch(directory)
+% png2poly_batch - Extracts boundaries of letter forms from PNG files as a
+% polygon
 
 smooth_iter = 2;
 max_points = 400;
@@ -9,17 +11,17 @@ letters = dir(directory);
 
 for f = 1:length(letters)
     
-    poly = {};
+    polygon = {};
     [pathstr,name,ext] = fileparts(letters(f).name);
     
     % Load the letter instance
     if(strcmp(ext,'.png'))
-        source = strcat(name,ext);
+        source = strcat(directory,name,ext);
         polygon_saveas = strcat(directory,'polygons/',name,'.mat');
     
         % Extract Boundary
-        poly = png2poly(source,smooth_iter,max_points);
-        save(polygon_saveas,'poly');
+        polygon = png2poly(source,smooth_iter,max_points);
+        save(polygon_saveas,'polygon');
     end
     
 end
