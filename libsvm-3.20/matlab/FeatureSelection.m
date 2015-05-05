c12e5d851cc649570b7b3894b843cedb75adb0da
Zeinab Sadeghipour <znbsadeghipour@gmail.com>
2015-05-04 21:04:12 -0700

    Updated ML part

--- libsvm-3.20/matlab/FeatureSelection.m
+++ libsvm-3.20/matlab/FeatureSelection.m
@@ -1,18 +1,25 @@
 clear all
 close all
 
-[training_label, training_data] = libsvmread('traindata.txt');
-[test_label, test_data] = libsvmread('testdata.txt');
+%[training_label, training_data] = libsvmread('traindata.txt');
+%[test_label, test_data] = libsvmread('testdata.txt');
+[all_label, all_data] = libsvmread('alldata.txt');
 
 param = ['-s 3 -t 2 -g ', num2str(0.5), ' -c ', num2str(0.5), ' -p 0.1 -q'];
 
 % criterion
-SVRwrapper = @(training_data, training_label, test_data, test_label)...
-    sum((svmpredict(test_label, test_data, svmtrain(training_label, training_data, param)) - test_label).^2);
+SVRwrapper = @(training_x, training_y, test_x, test_y)...
+    sum((svmpredict(test_y, test_x, svmtrain(training_y, training_x, param)) - test_y).^2);
 
 % feature selection
-X = [training_data; test_data];
-Y = [training_label; test_label];
-[selected_features, his] = sequentialfs(SVRwrapper, X, Y);
+% remember that for feature selection, you need to do leave-one-out,
+% otherwise, everytime you run, you get different features selected, it
+% also depends on the input test set
+X = all_data;
+Y = all_label;
+% X = [training_data; test_data];
+% Y = [training_label; test_label];
+[selected_features, his] = sequentialfs(SVRwrapper, X, Y, 'cv',35);
+sel_features = find(selected_features);
 
 save('..\..\letters and features\synthetic s\selected_features.mat', 'selected_features');
--- libsvm-3.20/matlab/preparedata.m
+++ libsvm-3.20/matlab/preparedata.m
@@ -11,28 +11,41 @@ load(fullfile(labels_folder, labels_file));
 load(fullfile(labels_folder, mapping_file));
 fsd_files = dir(fullfile(features_folder, 'fourier shape descriptors', '*.mat'));
 hmd_files = dir(fullfile(features_folder, 'Hu moments descriptor', '*.mat'));
+cc_files = dir(fullfile(features_folder, 'Chain code', '*.mat'));
+concavity_files = dir(fullfile(features_folder, 'concavity descriptors', '*.mat'));
 
 %producing training data
-test_size = 5;
+test_size = 6;
 training_size = size(fsd_files,1) - test_size;
-no_features = 67;
+no_features = 142;
 training_data = zeros(training_size,no_features);
 training_label = zeros(training_size,1);
 test_data = zeros(test_size,no_features);
 test_label = zeros(test_size,1);
+all_data = zeros(training_size + test_size, no_features);
+all_label = zeros(training_size + test_size, 1);
 
 counter = 1;
 
 for i = 1:size(fsd_files,1)
     load(fullfile(features_folder, 'fourier shape descriptors',fsd_files(i).name));
     load(fullfile(features_folder, 'Hu moments descriptor',hmd_files(i).name));
+    load(fullfile(features_folder, 'Chain code',cc_files(i).name));
+    load(fullfile(features_folder, 'concavity descriptors',concavity_files(i).name));
     
-    if mod(i,6) == 1
+    if mod(i,5) == 1
         test_data(counter, 1:60) = fsd;
-        test_data(counter, 61:67) = M;
+        test_data(counter, 61:85) = conc;
+        test_data(counter, 86:92) = M;
+        test_data(counter, 93:142) = cc_sampled;
+        all_data(i, :) = test_data(counter, :);
+        hmd_files(i).name
     else
         training_data(i - counter + 1, 1:60) = fsd;
-        training_data(i - counter + 1, 61:67) = M;
+        training_data(i - counter + 1, 61:85) = conc;
+        training_data(i - counter + 1, 86:92) = M;
+        training_data(i - counter + 1, 93:142) = cc_sampled;
+        all_data(i, :) = training_data(i - counter + 1, :);
     end
     
     [path, feature_file, ext] = fileparts(fsd_files(i).name);
@@ -40,12 +53,13 @@ for i = 1:size(fsd_files,1)
     feature_index = str2double(feature_split(1));
     for j = 1:length(labels)
         if feature_index == mapping(j)
-            if mod(i,6) == 1
+            if mod(i,5) == 1
                 test_label(counter) = labels(j);
                 counter = counter + 1;
             else
                 training_label(i - counter + 1) = labels(j);
             end
+            all_label(i) = labels(j);
         end
     end
 end
@@ -61,9 +75,12 @@ for i = 1:no_features
     if max ~= min
         training_data(:,i) = lower + (upper - lower) .* ((training_data(:,i) - repmat(min,training_size,1))./(max - min));
         test_data(:,i) = lower + (upper - lower) .* ((test_data(:,i) - repmat(min,test_size,1))./(max - min));
+%         all_data(:,i) = lower + (upper - lower) .* ((all_data(:,i) - repmat(min,test_size + training_size,1))./(max - min));
     end
 end
 
 % write the data into files
 libsvmwrite('traindata.txt', training_label, sparse(training_data));
 libsvmwrite('testdata.txt', test_label, sparse(test_data));
+libsvmwrite('alldata.txt', all_label, sparse(all_data));
+
